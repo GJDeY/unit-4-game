ad86eb899be73f4af80dadc139b18f3d2cd80eb1
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-08-08 23:50:37 -0500

    changed css path, still not working

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+
+
+
+    randomNumber = Math.floor(+ Math.random(1 - 12) + 1)
+        ;
+
+    wins = 0
+    losses = 0
+    winner = false
+    loser = false
+    numberGenerated = Math.randomNumber
+    cryOne = Math.random
+    cryTwo = Math.random + math.//current stored value
+        cryThree=Math.random + math.//current stored value
+            cryFour=Math.random + math. //current stored value
+
+                $("cryOne").click(function () {
+                    $("cryOne").addClass("blue");
+                });
+    //if (win) {
+    //    alert("😎 Winner!!! 😜")
+    //        ;
+    //} else {
+    //    alert("You lose 🤕!")
+    ;
+
+}
+
+    // if (numberAdded === randomNumber) {
+    //    alert("😎 Winner!!! 😜")
+    // </script>
+    }
+
+
+    ellipse(30, 100, 400)  ///testing if js is working
+
+    function loss() {
+
+        //start over, without refresh
+    }
+
+
+    //< script >
+  //  document.getElementById("cry1").innerHTML = "Magic Crystal Game!";
+</script >
+
+
+
+
+
+
+
+
+
+}
